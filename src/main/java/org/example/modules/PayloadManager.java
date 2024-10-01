3911472a5edc6045114d1348eb80214eaf2cb1c4
Maruf <marufsherikar32@gmail.com>
2024-10-01 15:31:17 +0530

    Framework strc with frst TC

--- src/main/java/org/example/modules/PayloadManager.java
+++ src/main/java/org/example/modules/PayloadManager.java
@@ -1,6 +1,49 @@
 package org.example.modules;
 
+import com.github.javafaker.Faker;
+import com.google.gson.Gson;
+import org.example.payloads.pojos.Booking;
+import org.example.payloads.pojos.Bookingdates;
+
+import java.util.Collections;
+
 public class PayloadManager {
+
+
+    public String createPayloadGSON() {
+        Faker faker = new Faker();
+        Booking booking = new Booking();
+        String expectFirstName = faker.name().firstName();
+
+
+        booking.setFirstname(expectFirstName);
+        booking.setLastname("Sherikar");
+        booking.setTotalprice(999);
+        booking.setDepositpaid(true);
+
+        Bookingdates bookingdates = new Bookingdates();
+        bookingdates.setCheckin("2018-01-01");
+        bookingdates.setCheckout("2018-01-01");
+        ;
+        booking.setBookingdates(bookingdates);
+        booking.setAdditionalneeds(Collections.singletonList("Breakfast"));
+
+        System.out.println(booking);
+        // object -> JSOn String (GSON)
+
+        Gson gson = new Gson();
+        String jsonStringBooking = gson.toJson(booking);
+        System.out.println(jsonStringBooking);
+        return jsonStringBooking;
+
+    }
+
+
+    public void updatePayload(){
+
+
+
+    }
 
 
 }
--- src/test/java/org/example/tests/crud/TCCreateBooking.java
+++ src/test/java/org/example/tests/crud/TCCreateBooking.java
@@ -1,12 +1,27 @@
 package org.example.tests.crud;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
+import org.example.base.BaseTest;
+import org.example.endpoints.APIConstants;
 import org.testng.annotations.Test;
 
-public class TCCreateBooking {
-    @Test
+public class TCCreateBooking extends BaseTest {
 
+
+    @Owner("Maruf")
+    @Description("Verify that the create booking with the valid payload, Status Code 200")
+    @Test
     public void testPositivePOSTReq(){
 
+
+        requestSpecification.basePath(APIConstants.CREATE_UPDATE_BOOKING_URL);
+        response = RestAssured.given().spec(requestSpecification)
+                .when().body(payloadManager.createPayloadGSON()).post();
+        validatableResponse = response.then().log().all();
+        validatableResponse.statusCode(200);
     }
 
 }
